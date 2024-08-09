75ca073f82213e524122c25c68fe0809d2f9a66d
Stanislav Bochok <stanislavbochok@gmail.com>
2024-08-09 21:27:22 +0300

    Изменение в app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+a = 5
+
 @app.route('/')
 def hello():
     return "module1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
-
-#1111
