e95ac1ab408c23a897998a405c20e2f9529dcf88
Stanislav Bochok <90556889+WTSTRILA@users.noreply.github.com>
2024-08-19 01:43:34 +0300

    Update test_minio.py

--- module2/PR2/test_minio.py
+++ module2/PR2/test_minio.py
@@ -24,46 +24,34 @@ def file_to_save(tmp_path: Path) -> Path:
 
 class TestMinioClientNative:
     def test_upload_file(self, minio_client_native: MinioClientNative, file_to_save: Path, tmp_path: Path):
-        # Upload file
         minio_client_native.upload_file(file_to_save)
         
-        # Download file
         path_to_save = tmp_path / "saved_file.mock"
         minio_client_native.download_file(object_name=file_to_save.name, file_path=path_to_save)
         
-        # Check if the file exists locally
         assert path_to_save.exists()
 
     def test_delete_file(self, minio_client_native: MinioClientNative, file_to_save: Path):
-        # Upload file
         minio_client_native.upload_file(file_to_save)
         
-        # Delete file
         minio_client_native.delete_file(file_to_save.name)
         
-        # Try to download the file to ensure it was deleted
         with pytest.raises(minio.error.S3Error):
             minio_client_native.download_file(object_name=file_to_save.name, file_path=Path("/dev/null"))
 
 class TestMinioClientS3:
     def test_upload_file(self, minio_client_s3: MinioClientS3, file_to_save: Path, tmp_path: Path):
-        # Upload file
         minio_client_s3.upload_file(file_to_save)
         
-        # Download file
         path_to_save = tmp_path / "saved_file.mock"
         minio_client_s3.download_file(object_name=file_to_save.name, file_path=path_to_save)
         
-        # Check if the file exists locally
         assert path_to_save.exists()
 
     def test_delete_file(self, minio_client_s3: MinioClientS3, file_to_save: Path):
-        # Upload file
         minio_client_s3.upload_file(file_to_save)
         
-        # Delete file
         minio_client_s3.delete_file(file_to_save.name)
         
-        # Try to download the file to ensure it was deleted
         with pytest.raises(FileNotFoundError):
             minio_client_s3.download_file(object_name=file_to_save.name, file_path=Path("/dev/null"))
